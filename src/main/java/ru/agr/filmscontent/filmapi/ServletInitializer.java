e38eb592f49c5bcb122a743d5aa4c58a73a67fec
Arslan Rabadanov <Agrabadanov@gmail.com>
2020-02-07 14:39:08 +0300

    init

---
+++ src/main/java/ru/agr/filmscontent/filmapi/ServletInitializer.java
@@ -0,0 +1,13 @@
+package ru.agr.filmscontent.filmapi;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(FilmapiApplication.class);
+	}
+
+}
