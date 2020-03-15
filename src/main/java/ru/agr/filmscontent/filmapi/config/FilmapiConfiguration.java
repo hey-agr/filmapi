c72a82d3ec8f985aaff7fec8fb3f5ac71f4b34eb
github_main <AGRabadanov@gmail.com>
2020-03-15 16:42:25 +0300

    CORS

--- src/main/java/ru/agr/filmscontent/filmapi/config/FilmapiConfiguration.java
+++ src/main/java/ru/agr/filmscontent/filmapi/config/FilmapiConfiguration.java
@@ -8,6 +8,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -50,5 +52,15 @@ public class FilmapiConfiguration {
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.regex("(?!/error.*).*"))
                 .build();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/movies").allowedOrigins("http://127.0.0.1:5500");
+            }
+        };
     }
 }
